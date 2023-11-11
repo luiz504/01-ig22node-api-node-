22faec15df14248ec65f4e2e59b635820586ff64
Luiz Bueno <luiz504@hotmail.com>
2023-11-11 14:37:59 -0300

    feat(transactions): setup transaction migrations

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import setupKnex from 'knex'
+import setupKnex, { Knex } from 'knex'
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
-})
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+  useNullAsDefault: true,
+}
+
+export const knex = setupKnex(config)
