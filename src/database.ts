753dd57566c1a87dbe7f270ea0f68beffe3873fb
Luiz Bueno <luiz504@hotmail.com>
2023-11-14 17:32:29 -0300

    chore: production preparation

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,16 @@ import setupKnex, { Knex } from 'knex'
 
 import { env } from './env'
 
+const connection =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
